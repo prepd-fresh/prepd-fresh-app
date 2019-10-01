77bb0e0365720799cd6b9c6efcae518584270b80
DustinGaudet <dustin@dustingaudet.com>
2019-10-01 19:08:46 -0400

    Remove test output heading

--- src/App.web.js
+++ src/App.web.js
@@ -6,7 +6,6 @@ import {
 } from 'react-stripe-elements';
 
 const App = () => (
-  // <h1>Hello world from web!</h1>
   <StripeProvider apiKey="pk_test_dxJRiJo1wDpI8NWpKyTy9WII00GF5Wl5rQ">
     <Elements>
       <CheckoutForm />

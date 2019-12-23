7fb8a5be94afd3cb9dd106c713ebefbc715f0ad7
DustinGaudet <dustin@dustingaudet.com>
2019-12-23 15:20:56 -0500

    Add react-native-elements & Roboto font

--- App.js
+++ App.js
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { Provider } from "react-redux";
 import prepdApp from "./src/reducers";
 import preloadedState from "./src/preloadedState";
 import Main from "./src/components/Main";
+import * as Font from "expo-font";
 
 const store = createStore(prepdApp, preloadedState, applyMiddleware(thunk));
 
-const App = () => (
-  <Provider store={store}>
-    <Main />
-  </Provider>
-);
+const App = () => {
+  useEffect(() => {
+    Font.loadAsync({
+      Roboto: require("./assets/fonts/Roboto-Regular.ttf")
+    });
+  });
+  return (
+    <Provider store={store}>
+      <Main />
+    </Provider>
+  );
+};
 
 export default App;
 

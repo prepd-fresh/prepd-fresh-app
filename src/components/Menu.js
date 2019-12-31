5704d6b9940cb6864611c4f10b2ad60d4c6bf91e
DustinGaudet <dustin@dustingaudet.com>
2019-12-31 01:24:55 -0500

    Filter out "outofstock" items from menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,32 +3,36 @@ import { Text, View, ScrollView } from "react-native";
 import styled from "styled-components/native";
 import MealCard from "./MealCard";
 
-const isProductType = desiredType => product => desiredType === product.type;
+const isAvailableAndProductType = desiredType => product =>
+  product["stock_status"] !== "outofstock" && desiredType === product.type;
+
 const toMealCardFromMealWith = sizeVariants => meal => (
   <MealCard key={meal.id} {...{ ...meal, sizeVariants }} />
 );
 
-const Menu = ({ products, nextWeekdayDate, ...props }) => (
-  <ScrollView
-    contentContainerStyle={{
-      padding: 20,
-      justifyContent: "flex-start",
-      display: props.cartIsNotVisible ? "flex" : "none"
-    }}
-  >
-    <MenuHed>Menu</MenuHed>
-    <Text>Choose from our weekly rotating selection of dishes!</Text>
-    <Text>
-      Meals are delivered every Sunday. The next delivery date is
-      {" " + nextWeekdayDate(7)}.
-    </Text>
-    <View style={{ marginBottom: 100 }}>
-      {Object.values(products)
-        .filter(isProductType("meal"))
-        .map(toMealCardFromMealWith(props.productSizeVariants))}
-    </View>
-  </ScrollView>
-);
+const Menu = ({ products, nextWeekdayDate, ...props }) => {
+  const meals = Object.values(products)
+    .filter(isAvailableAndProductType("meal"))
+    .map(toMealCardFromMealWith(props.productSizeVariants));
+
+  return (
+    <ScrollView
+      contentContainerStyle={{
+        padding: 20,
+        justifyContent: "flex-start",
+        display: props.cartIsNotVisible ? "flex" : "none"
+      }}
+    >
+      <MenuHed>Menu</MenuHed>
+      <Text>Choose from our weekly rotating selection of dishes!</Text>
+      <Text>
+        Meals are delivered every Sunday. The next delivery date is
+        {" " + nextWeekdayDate(7)}.
+      </Text>
+      <View style={{ marginBottom: 100 }}>{meals}</View>
+    </ScrollView>
+  );
+};
 
 const MenuHed = styled.Text`
   color: #3e444b;
